01bd40b2bc4a117871139fc62a71c321d18b43f6
Halldór Hrafn Reynisson <halldor.hrafn.reynisson@gmail.com>
2022-10-20 10:42:26 +0000

    Minor changes to CSS and routes

--- routes/user.js
+++ routes/user.js
@@ -6,13 +6,17 @@ router.get('/', (req, res) => {
   const title = 'Title';
   let loggedin = false;
   let username = '';
+  let admin = false;
 
   if (req.session.loggedin) {
     loggedin = true;
     username = req.session.username;
+    admin = req.session.admin;
   }
 
-  res.render('user', { title, loggedin, username });
+  res.render('user', {
+    title, loggedin, username, admin,
+  });
 });
 
 module.exports = router;
